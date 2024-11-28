c24c1b93eef5df6eac51be7a6e00a6b01f525095
AlbaVidalEsteve <66486832+AlbaVidalEsteve@users.noreply.github.com>
2024-11-28 11:14:00 +0100

    Crear mvc login1: controlador credanciales hardcode

---
+++ LogInMVC/Controllers/AuthentificationController.cs
@@ -0,0 +1,31 @@
+﻿using LogInMVC.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LogInMVC.Controllers
+{
+    public class AuthentificationController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Validar usuari
+                if (model.Username == "admin" && model.Password == "password")
+                {
+                    //Autentificación exitosa
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Usuario o contraseña incorrectos.");
+            
+            }
+            return View(model);
+        }
+    }
+}
---
+++ LogInMVC/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LogInMVC.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "El nombre de usuario es obligatorio.")]
+        [Display(Name ="Nombre del usuario")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es obligatoria.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+    }
+}
